69030888b6b7fd0bbb47b4f79f3d5c1d85bd6cf6
Kangoor Brown <kangoorbrown@wp.pl>
2025-10-29 19:44:32 +0100

    2025-10-29: solved 'Everything' excercise

---
+++ excercises/5-higher-order-functions/everything.js
@@ -0,0 +1,18 @@
+// 1. Loop
+const every = (array, test) => {
+  for (let i=0; i < array.length; i++) {
+    if (!test(array[i])) return false;
+  }
+  return true;
+}
+
+// 2. Some
+const every = (array, test) => !array.some(el => !test(el));
+
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
